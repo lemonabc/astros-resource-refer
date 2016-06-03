ed6632b54e75d8eea8be9a6ecbed08765bcc81bb
tiger <wanhu88@gmail.com>
2016-06-03 15:16:05 +0800

    完善JS引用图片

--- astro-resource-refer.js
+++ astro-resource-refer.js
@@ -1,17 +1,14 @@
 'use strict';
-
 var util = require('util');
 require('astros');
-
 /**
  * 读取文件时，替换JS和CSS中的图片引用
  */
-
 module.exports = new astro.Middleware({
     fileType: ['js', 'css']
 }, function(asset, next) {
     let data = asset.data
-    if(!data){
+    if (!data) {
         next(asset);
         return;
     }
@@ -19,57 +16,56 @@ module.exports = new astro.Middleware({
     let self = asset;
     let imgPrefix = prjCfg.imgPath || prjCfg.cdnPrefix || '';
     // 替换CSS中的图片路径
+    // 使用相对路径
     if (asset.fileType == 'css') {
-        data = data.replace(/(url|\.img)\([\'\"]?(?!http)(?!\~)([^\'\"\)]*?)[\'\"]?\s*?(\)|\,)/g,
-            function(str, urlstr, imgpath, c) {
-                if (asset.modNameMin[self.modType]) {
-                    return util.format('%s("%s/img/%s/%s/%s"%s', urlstr, 
-                        imgPrefix, asset.modNameMin[self.modType], self.name, imgpath, c);
-                } else {
-                    return str;
-                }
-            });
-        data = data.replace(/(url|\.img)\(([\'\"]?)(?!http)~(.*?)\)/g,
-            function(str, urlstr, qm, imgpath) {
-                return util.format('%s(%s%s%s)', urlstr, qm, imgPrefix, imgpath);
-            });
+        data = data.replace(/(url|\.img)\([\'\"]?(?!http)(?!\~)([^\'\"\)]*?)[\'\"]?\s*?(\)|\,)/g, function(str, urlstr, imgpath, c) {
+            if (asset.modNameMin[self.modType]) {
+                return util.format('%s("%s/img/%s/%s/%s"%s', urlstr, imgPrefix, asset.modNameMin[self.modType], self.name, imgpath, c);
+            } else {
+                return str;
+            }
+        });
+        data = data.replace(/(url|\.img)\(([\'\"]?)(?!http)~(.*?)\)/g, function(str, urlstr, qm, imgpath) {
+            return util.format('%s(%s%s%s)', urlstr, qm, imgPrefix, imgpath);
+        });
         //@fontPath:"~/img/font"
-        data = data.replace(/[\'\"](?!http)~([^~]*?)[\'\"]/g,
-            function(str, imgpath) {
-                return util.format('"%s%s"', imgPrefix, imgpath);
-            });
+        data = data.replace(/[\'\"](?!http)~([^~]*?)[\'\"]/g, function(str, imgpath) {
+            return util.format('"%s%s"', imgPrefix, imgpath);
+        });
     }
     if (asset.fileType == 'js') {
-        let regexp, adorn;
-        if(!prjCfg._JIRExp){
-            let rule = prjCfg.jsImgRefer || {};
-                rule = rule.rule;
-            if(!rule){
+        if (!prjCfg._JIRExp) {
+            let rule = prjCfg.jsImgRefer ? prjCfg.jsImgRefer.rule : (this.config.jsImgRefer);
+            if (!rule) {
                 rule = '$res(.__path__.)';
-                // console.warn('astro-resource-refer:','项目未配置 jsImgRefer.rule 字段');
+                // console.warn('astro-resource-refer:', '项目未配置 jsImgRefer.rule 字段');
                 // next(asset);
                 // return;
             }
-            prjCfg._JIRAdorn = rule.split('__path__');
+            // prjCfg._JIRAdorn = rule.split('__path__');
             rule = rule.
-                replace(/([$^()/?])/ig, function(a, b){
-                    return '\\'+b;
-                });
-            prjCfg._JIRExp = new RegExp(rule.replace('__path__','(.*?)'), 'ig');
+            replace(/([$^()/?])/ig, function(a, b) {
+                return '\\' + b;
+            });
+            prjCfg._JIRExp = new RegExp(rule.replace('__path__', '(.*?)'), 'ig');
         }
-        regexp  = prjCfg._JIRExp;
-        adorn   = prjCfg._JIRAdorn;
-        data    = data.replace(regexp,function(str, imgpath) {
-            if(imgpath.charAt(0) == '~'){
-                return util.format('%s%s', imgPrefix, imgpath.substr(1))
+        let urlPerfix;
+        if(prjCfg.host){
+            // 开发访问模式，直接拼接上 ip和端口号
+            urlPerfix = 'http://'+prjCfg.host + imgPrefix;
+        }else{
+            urlPerfix = this.config.jsImgPath || imgPrefix;
+        }
+        data = data.replace(prjCfg._JIRExp, function(str, imgpath) {
+            if (imgpath.charAt(0) == '~') {
+                return util.format('"%s%s"',urlPerfix, imgpath.substr(1))
             }
             if (asset.modNameMin[self.modType]) {
-                return util.format('"%s/img/%s/%s/%s"',
-                    imgPrefix, asset.modNameMin[self.modType], self.name, imgpath);
+                return util.format('"%s/img/%s/%s/%s"', urlPerfix, asset.modNameMin[self.modType], self.name, imgpath);
             } else {
                 return str;
-            }                
-            return ;
+            }
+            return;
         });
     }
     asset.data = data;
--- readme.md
+++ readme.md
@@ -1,85 +1,48 @@
-## 说明
+astros中间件
 
 扩展JS及CSS中的资源引用。
 
-资源引用分为绝对路径和相对路径。
-
-
-### 绝对路径
-
-解析时，就把图片路径中的“~”换成图片目录。开发环境默认图片目录是 `assets`
-
-#### CSS：
-
-    background:url(~/img/logo.png);
-    /*
-        output: 
-        background:url(/assets/img/logo.png)
-    */
-
-#### JS:
-    
-    var logoSrc = $res("~/img/logo.png");
-    /*
-        output:
-        var logoSrc = "/assets/img/logi.png"
-    */  
-    
-##### JS 中的样式引用语法可自定义
-
-参见 site.js中jsImgRefer字段，如：
-
+## 配置
 ```
-    jsImgRefer : {
-        rule : '\"#path__\"'
+{
+    name: 'astros-resource-refer',
+    config: {
+        jsImgRefer:'$res(.__path__.)',
+        jsImgPath : './'
     }
+}
 ```
 
-JS中你可写成
+`jsImRefer`表示JS引用资源的语法，默认值如示例。其中 `__path__`是固定格式，代表图片路径。如 `$.res(__path__)`或`%__path__%`。
 
-```
-    var logoSrc = "#~/img/logo.png";
-    // 解析后
-    var logoSrc = "/img/logo.png";
-    // 相对路径
-    var logoSrc = "#logo.png";
-    // 解析后
-    var logoSrc = "/img/p/home/logo.png";    
-```
+JS中引用的图片，在HTML上展示时，如果使用相对路径，会造成404（图片在静态资源服务器上）。
 
+因此，在开发过程中，中间件会自动加上静态资源服务器的域名补全图片的路径。 发布后，如果静态资源和Web服务同服务器，可通过`jsImaPath`参数设置引用图片的相对路径，如果静态资源是独立的服务器，则可通过它设置静态资源服务器域名。
 
-### 相对路径
+## 引用本页面（模块或组件）的图片
+该功能通过中间件 `astro-resource-refer` 实现
 
-Astro把资源划分为3类，分别是页面（page）、网页组件（webCom）、JS组件（jsCom）。
+html结构、样式、JS中均有可能引用到图片。
 
-他们的文件结构都是一样的，一home页面为例：
+在html结构中，可通过'$res'方法本页面（模块、组件）下的图片资源：
 
-* home
-    * home.html
-    * home.less
-    * home.js
-    * [dir]img
-        * banner.jpg
+    <img src="$res(avatar.png)" >
+    
+>是的，它相对引用
 
-页面自身的图片均存放在img目录中。在LESS和JS中，需要引用图片时，直接写图片名称即可。
-#### CSS
+**样式中：**
 
-    background:url(banner.jpg);
-    /*
-        output: 
-        background:url(/assets/img/p/home/logo.png)
-    */
-JS中同理。
+    background:url(avatar.png) no-repeat;
+    
+**js中：**
 
-页面、网页组件、JS组件最终图片引用路径如下：
+    var avatar = $res('avatar.png');
+    
 
-| 资源所属模块    | 模块名称  |   图片名称       | 发布后路径 |
-|:----------|:---------|:-----------:| ---------:|
-| 网页        |  home    | logo.png       |  /assets/img/p/home/logo.png |
-| 网页组件     |  header  | logo.png        |  /assets/img/wc/header/logo.png |
-| JS组件      |  dialog  | logo.png       |  /assets/img/jc/dialog/logo.png |
+### 公共图片
 
+除了页面（模块、组件）自有的图片，还有logo，icon等公共图片，这些都需要存放在 root/asset/img目录下，引用时，`在URL前面加上 ~/即可`。以HTML为例：
 
-
-
-<br/><br/><br/><br/><br/>
+    <img src="$res(~/img/avatar.png)" >
+    
+样式和JS中同理。
